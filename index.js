0534f9015b2a3455a3aab4c8e8b9c2555a92eaaa
Miina Saroma <miinasaroma@gmail.com>
2020-08-09 21:26:32 +0300

    3.4 - 3.6: Handle POST, DELETE for a person, handle errors

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express')
+const { response } = require('express')
 const app = express()
+
+//tän unohdin!
+app.use(express.json())
 
 let persons = [
   {
@@ -42,8 +46,54 @@ app.get('/api/persons/:id', (req, res) => {
   if (person) {
     res.json(person)
   } else {
-    res.send(404).end()
+    res.status(404).end()
   }
+})
+
+//should we check for used ids? maybe not
+const generateId = () => {
+  return Math.floor(Math.random() * Math.floor(5000))
+}
+
+const handleError = (res, statuscode, message) => {
+  return res.status(statuscode).json({
+    error: `${message}`
+  })
+}
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+  //console.log('body ', body)
+
+  //TODO? if both are missing, combine the message? 
+  if (!body.name) {
+    return handleError(res, 400, "name missing")
+  }
+
+  if (!body.number) {
+    return handleError(res, 400, "number missing")
+  }
+
+  if (persons.find(p => p.name === body.name)) {
+    return handleError(res, 400, "name must be unique")
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  }
+
+  //console.log('person ', person)
+  persons = persons.concat(person)
+
+  res.json(person)
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(p => p.id !== id)
+  res.status(204).end()
 })
 
 const port = 3001
