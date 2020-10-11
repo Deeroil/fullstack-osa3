7e8a008f33316c9a70ba35e56265c5fb2f1f0e34
Miina Saroma <miinasaroma@gmail.com>
2020-10-11 19:23:06 +0300

    Handle errors using middleware

--- index.js
+++ index.js
@@ -17,34 +17,37 @@ morgan.token('body', function (req, res) {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-app.get('/info', (req, res) => {
-  Person.countDocuments().then(result => {
-    res.send(
-      `<div>Phonebook has info for ${result} people</div>
+app.get('/info', (req, res, next) => {
+  Person.countDocuments()
+    .then(result => {
+      res.send(
+        `<div>Phonebook has info for ${result} people</div>
        <div>${new Date().toString()}</div>`)
-  })
+    })
+    .catch(error => next(error))
 })
 
-app.get('/api/persons', (req, res) => {
-  Person.find({}).then(result => {
-    res.json(result)
-  })
+app.get('/api/persons', (req, res, next) => {
+  Person.find({})
+    .then(result => {
+      res.json(result)
+    })
+    .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (req, res) => {
-  Person.findById(req.params.id).then(person => {
-    if (person) {
-      res.json(person)
-    } else {
-      res.status(404).end()
-    }
-  }).catch(error => {
-    console.log(error)
-    res.status(400).send({ error: 'malformatted id' })
-  })
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-const handleError = (res, statuscode, message) => {
+const handleContentError = (res, statuscode, message) => {
   return res.status(statuscode).json({
     error: `${message}`
   })
@@ -55,11 +58,11 @@ app.post('/api/persons', (req, res) => {
 
   //TODO? if both are missing, combine the message? 
   if (!body.name) {
-    return handleError(res, 400, "name missing")
+    return handleContentError(res, 400, "name missing")
   }
 
   if (!body.number) {
-    return handleError(res, 400, "number missing")
+    return handleContentError(res, 400, "number missing")
   }
 
   const person = new Person({
@@ -72,17 +75,33 @@ app.post('/api/persons', (req, res) => {
   })
 })
 
-app.delete('/api/persons/:id', (req, res) => {
+app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndRemove(req.params.id)
-  .then(result => {
-    res.status(204).end()
-  })
-  .catch(error => {
-    console.log(error)
-    console.log('error in removing')
-  })
-  
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => {
+      console.log('error in removing')
+      next(error)
+    })
 })
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, req, res, next) => {
+  console.log(error)
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })   //!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  }
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
